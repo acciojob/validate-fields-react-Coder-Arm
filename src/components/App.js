f7d0c99ebeabe10500a8bbb2f70743d037e31d70
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-28 13:20:30 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [name ,setName] = useState("");
+   const [pass,setPass] = useState("");
+   const [error,setError] = useState("");
+
+   function formHandler(e){
+     e.preventDefault();
+     if(!name || !pass){
+      setError("Both username and password are required.")
+     }
+     else{
+      setError("")
+      setName("");
+      setPass("");
+     }
+   }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+     <form onSubmit={formHandler}>
+      <label htmlFor="name">Username : <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value) }/></label>
+      <label htmlFor="pass">Password : <input type="password" id="pass" value={pass} onChange={(e) => setPass(e.target.value) }/></label>
+      {error && <div id="errorMessage">{error}</div>}
+      <button>Login</button>
+     </form>
     </div>
   )
 }
